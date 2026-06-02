181f0ab553564edc322a7bfbe2ce5c5a7e681a89
Yaswant <yaswantsoni2005@gmail.com>
2026-06-02 13:23:04 +0530

    backenbd url updated

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import type { AxiosError, AxiosResponse, AxiosRequestConfig } from 'axios';
 
+const API_BASE_URL = 'https://server-kg-z497.onrender.com/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
 // const API_BASE_URL = 'https://server-kg.onrender.com/api';
-const API_BASE_URL = 'https://server-kg.onrender.com/api';
 
 // Create an axios instance with default config
 const apiClient = axios.create({
